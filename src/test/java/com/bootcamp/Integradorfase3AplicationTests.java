4a7afd03c2fc9bc21f56a9f10e940a938894721b
Emma Albarran Villasmil <ealbarran@kdu.cl>
2024-03-10 12:03:58 -0300

    estructura inicial

---
+++ src/test/java/com/bootcamp/Integradorfase3AplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integradorfase3Aplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
